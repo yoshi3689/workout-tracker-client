75aa315c9aa86ab6d4c4ad618ebf86bb81c5c3cb
Yoshiyuki Nagai <ynagai@my.bcit.ca>
2023-12-11 14:02:55 -0800

    change base url for axios

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = 'http://localhost:5001/';
+export const BASE_URL = process.env.NODE_ENV === "production" ? 'https://www.yn-track-wo.xyz/' : 'http://localhost:5001/';
 
 export const request = axios.create({
   baseURL: BASE_URL,
