75fde05adf7aa835a51c5543acd95f9aad226259
Yoshiyuki Nagai <ynagai@my.bcit.ca>
2024-01-13 18:55:45 -0800

    switch request URL for differet environments at runtime

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { REQUEST_COMMON_PREFIX } from "../utils/pathnames";
 
-export const BASE_URL = process.env.REACT_APP_REQUEST_URL;
+export const BASE_URL = (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? process.env.REACT_APP_REQUEST_URL : process.env.REACT_APP_REQUEST_URL_PROD;
 
 export const request = axios.create({
   baseURL: BASE_URL + REQUEST_COMMON_PREFIX,
